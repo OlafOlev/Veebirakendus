b0c450d721aab432fc1b0ad1c8c9542fa51b7e7e
OlafOlev <oalfolev@gmail.com>
2023-06-10 18:53:03 +0300

    Veeb

---
+++ index.ts
@@ -0,0 +1,27 @@
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import stringsController from "./controllers/strings";
+import productsController from "./controllers/products";
+import productListController from "./controllers/productlist";
+import parcelController from "./controllers/parcelmachines";
+
+const app: Express = express();
+
+app.use(cors({
+    origin: ['http://localhost:3006']
+}));
+app.use(bodyParser.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Express + TypeScript Server');
+});
+
+app.use('/', stringsController);
+app.use('/', productsController);
+app.use('/', productListController);
+app.use('/', parcelController);
+
+app.listen(3000,() => {
+    console.log(`[Server]: Server is running at http://localhost:3000`);
+});
